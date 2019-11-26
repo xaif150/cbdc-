b8e671a8ab032baf2bb77e5d6dcdd072837da933
xaif150 <xaif150@gmail.com>
2019-11-26 12:38:44 +0600

    add data

---
+++ app/src/main/java/com/cbdc/instagram_login/loged.java
@@ -0,0 +1,14 @@
+package com.cbdc.instagram_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loged extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loged);
+    }
+}
